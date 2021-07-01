638e117dbe452545db02560001116d5eaec04d19
Chen Gang G <gang.g.chen@intel.com>
2021-07-01 09:45:10 +0800

    Add basic x86 definition and implementation
    1. interrupt descriptor table(IDT) and interrupt service routine(ISR)
    2. global description table(GDT)
    3. floating point uint(FPU)
    4. exception handler
    
    Signed-off-by: Chen Gang G <gang.g.chen@intel.com>

---
+++ core/arch/x86_64/include/asm.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2008-2013 Travis Geiselbrecht
+ */
+#ifndef ASM_H
+#define ASM_H
+
+#define FUNCTION(x) .global x; .type x,STT_FUNC; x:
+#define DATA(x) .global x; .type x,STT_OBJECT; x:
+
+#define LOCAL_FUNCTION(x) .type x,STT_FUNC; x:
+#define LOCAL_DATA(x) .type x,STT_OBJECT; x:
+
+#endif
+
---
+++ core/arch/x86_64/include/kernel/fault.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2009 Corey Tabaka
+ * Copyright (c) 2018 Intel Corporation
+ */
+
+#ifndef FAULT_H
+#define FAULT_H
+
+void x86_exception_handler(x86_iframe_t *frame);
+
+#endif
