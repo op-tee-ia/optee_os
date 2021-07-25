1b1cbc1e5417fbbbabd2233ea7103ea97b7d8469
Chen Gang G <gang.g.chen@intel.com>
2021-07-25 20:30:33 +0800

    Add x86 spin_lock descriptor and update kernel/sub.mk
    1. Add x86 descriptor
    2. add spin_lock x86 implementation
    3. Add tee_time_ree.c and link.ld.S back
    4. Add trace enable/disable
    
    Signed-off-by: Chen Gang G <gang.g.chen@intel.com>

--- core/arch/x86_64/kernel/sub.mk
+++ core/arch/x86_64/kernel/sub.mk
@@ -1,57 +1,25 @@
 srcs-$(CFG_WITH_USER_TA) += ldelf_loader.c
 srcs-y += tee_time.c
-srcs-y += rpc_io_i2c.c
 srcs-y += otp_stubs.c
-srcs-y += delay.c
+srcs-y += descriptor.c
+srcs-y += fault.c
+srcs-y += gdt.S
+srcs-y += exceptions.S
+srcs-y += fpu.c
 
-srcs-$(CFG_SECURE_TIME_SOURCE_CNTPCT) += tee_time_arm_cntpct.c
 srcs-$(CFG_SECURE_TIME_SOURCE_REE) += tee_time_ree.c
-srcs-$(CFG_ARM64_core) += timer_a64.c
 
-srcs-$(CFG_ARM32_core) += spin_lock_a32.S
-srcs-$(CFG_ARM64_core) += spin_lock_a64.S
+srcs-y += spin_lock_64.S
 srcs-$(CFG_TEE_CORE_DEBUG) += spin_lock_debug.c
-srcs-$(CFG_ARM32_core) += tlb_helpers_a32.S
-srcs-$(CFG_ARM64_core) += tlb_helpers_a64.S
-srcs-$(CFG_ARM64_core) += cache_helpers_a64.S
-srcs-$(CFG_ARM32_core) += cache_helpers_a32.S
-srcs-$(CFG_PL310) += tz_ssvce_pl310_a32.S
-srcs-$(CFG_PL310) += tee_l2cc_mutex.c
 
-srcs-$(CFG_ARM32_core) += thread_a32.S
-srcs-$(CFG_ARM64_core) += thread_a64.S
+srcs-y += thread_64.S
 srcs-y += thread.c
-ifeq ($(CFG_CORE_FFA),y)
-srcs-y += thread_spmc.c
-cppflags-thread_spmc.c-y += -DTEE_IMPL_GIT_SHA1=$(TEE_IMPL_GIT_SHA1)
-srcs-$(CFG_ARM32_core) += thread_spmc_a32.S
-srcs-$(CFG_ARM64_core) += thread_spmc_a64.S
-else
 srcs-y += thread_optee_smc.c
-srcs-$(CFG_ARM32_core) += thread_optee_smc_a32.S
-srcs-$(CFG_ARM64_core) += thread_optee_smc_a64.S
-endif
-srcs-y += abort.c
-srcs-$(CFG_WITH_VFP) += vfp.c
-ifeq ($(CFG_WITH_VFP),y)
-srcs-$(CFG_ARM32_core) += vfp_a32.S
-srcs-$(CFG_ARM64_core) += vfp_a64.S
-endif
 srcs-y += trace_ext.c
-srcs-$(CFG_ARM32_core) += misc_a32.S
-srcs-$(CFG_ARM64_core) += misc_a64.S
-srcs-$(CFG_WITH_STMM_SP) += stmm_sp.c
-srcs-$(CFG_SECURE_PARTITION) += secure_partition.c
-srcs-$(CFG_SECURE_PARTITION) += spmc_sp_handler.c
+srcs-y += misc.c
 
 srcs-y += boot.c
-srcs-$(CFG_ARM32_core) += entry_a32.S
-srcs-$(CFG_ARM64_core) += entry_a64.S
-
-ifeq ($(CFG_UNWIND),y)
-srcs-$(CFG_ARM32_core) += unwind_arm32.c
-srcs-$(CFG_ARM64_core) += unwind_arm64.c
-endif
+srcs-y += entry_64.S
 
 srcs-$(CFG_VIRTUALIZATION) += virtualization.c
 
@@ -59,15 +27,6 @@ srcs-y += link_dummies_paged.c
 srcs-y += link_dummies_init.c
 
 asm-defines-y += asm-defines.c
-# Reflect the following dependencies:
-# asm-defines.c includes <kernel/thread.h>
-#   <kernel/thread.h> includes <asm.h>
-#     <asm.h> includes <generated/arm32_sysreg.h>
-#                  and <generated/arm32_gicv3_sysreg.h> (optional)
-asm-defines-asm-defines.c-deps += $(out-dir)/core/include/generated/arm32_sysreg.h
-ifeq ($(CFG_ARM_GICV3),y)
-asm-defines-asm-defines.c-deps += $(out-dir)/core/include/generated/arm32_gicv3_sysreg.h
-endif
 
 ifeq ($(CFG_SYSCALL_FTRACE),y)
 # We would not like to profile thread.c file as it provide common APIs
