e936a6607360660033e3d34203a81034d58a8b44
Jingdong Lu <jingdong.lu@intel.com>
2023-08-15 17:24:53 +0800

    Support Clang compiling for x86
    Signed-off-by: Jingdong Lu <jingdong.lu@intel.com>

--- core/arch/x86_64/kernel/fault.c
+++ core/arch/x86_64/kernel/fault.c
@@ -41,7 +41,7 @@ static void dump_fault_frame(x86_iframe_t *frame)
 	EMSG("errc: %16lx CR2: %16lx\n", frame->err_code, x86_get_cr2());
 }
 
-static void exception_die(x86_iframe_t *frame, const char *msg)
+static void __noreturn exception_die(x86_iframe_t *frame, const char *msg)
 {
 	trace_ext_puts(msg);
 	dump_fault_frame(frame);
@@ -52,7 +52,7 @@ static void exception_die(x86_iframe_t *frame, const char *msg)
 	}
 }
 
-static void x86_gpf_handler(x86_iframe_t *frame)
+static void __noreturn x86_gpf_handler(x86_iframe_t *frame)
 {
 	exception_die(frame, "unhandled gpf, halting\n");
 }
@@ -70,7 +70,7 @@ static void x86_invop_handler(x86_iframe_t *frame)
 	}
 }
 
-static void x86_unhandled_exception(x86_iframe_t *frame)
+static void __noreturn x86_unhandled_exception(x86_iframe_t *frame)
 {
 	EMSG("vector %u\n", (unsigned int)frame->vector);
 	exception_die(frame, "unhandled exception, halting\n");
